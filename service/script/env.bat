5e3e302034440ad34289b59367584b9a6a326e32
Linfeng Song <linfengsong@yahoo.com>
2024-02-19 21:54:01 -0500

    init project

--- service/script/env.bat
+++
@@ -1,1 +0,0 @@
-set PATH=C:\Program Files\java\jdk-1.8\bin;C:\linfeng\work-sh\sbt\bin;C:\software\windows-amd64;%PATH%
--- swag/script/build.bat
+++
@@ -1,8 +0,0 @@
-set ScriptPath=%~dp0
-set ScriptPath=%ScriptPath:~0,-1%
-
-pushd %ScriptPath%\..
-set srcDir=%CD%
-popd
-
-docker build -t swag:latest .
